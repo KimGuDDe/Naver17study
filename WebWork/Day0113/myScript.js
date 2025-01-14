4cb186fc9269e7f7a4e608c73ae7a0bd07b0b04e
KimNokYob <nok_yob@naver.com>
2025-01-14 19:35:53 +0900

    20250114

---
+++ WebWork/Day0113/myScript.js
@@ -0,0 +1,32 @@
+window.onload=function(){
+    let mycar=document.querySelector(".mycar");//배열타입
+    //0번이미지 이벤트
+    //마우스를 올리면 ../Photo/K-052.png 로 변경하고 벗어나면 다시 원래이미지
+    mycar[0].onmouseover=function(){
+        photo=this.getAttribute("src","");
+        this.setAttribute("src","../Photo/K-052.png");
+    }
+    mycar[0].onmouseout=function(){
+        this.setAttribute("src",photo)
+    }
+    //1번 이미지 이벤트
+    //마우스를 올리면 border를 알아서 설정하고 벗어나면 다시 없애기
+    mycar[1].onmouseover=function(){
+        this.style.borderStyle="inset";
+        this.style.borderWidth="10px";
+        this.style.borderColor="green";
+    }
+    mycar[1].onmouseout=function(){
+        this.style.border="none";
+    }
+    //2번 이미지 이벤트
+    //마우스를 올리면 .happy를 적용하고 벗어나면 다시 없애기
+    //.happy는 미리 만들어둘것(내용 상관X)
+    mycar[2].onmouseover=function(){
+        this.setAttribute("class","Mycar happy");
+    }
+    mycar[2].onmouseout=function(){
+        this.setAttribute("class","Mycar");
+    }
+
+}
